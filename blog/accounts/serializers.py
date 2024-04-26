a4d99784fda9d47c36d6ec3b875d0f7fd106b663
Kabhilan <kabhilan05@gmail.com>
2024-04-26 11:58:01 +0530

    Register api is created

--- blog/accounts/serializers.py
+++ blog/accounts/serializers.py
@@ -2,10 +2,19 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class RegisterSerializr(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
     username = serializers.CharField()
     password = serializers.CharField()
 
     def validate(self, data):
-        if User.objects.filter(username = data['username']).exists():
-            raise serializers.ValidationError('Username is already Taken')
+        username = data.get('username').lower()
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError('Username is already taken')
         return data
+    def create(self, validated_data):
+        user = User.objects.create(first_name = validated_data['first_name'],
+                                   last_name = validated_data['last_name'],
+                                   username = validated_data['username'].lower())
+        user.set_password(validated_data['password'])
+        return user
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializr
+from rest_framework import status 
 # Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        try:
+            data = request.data
+            serializer = RegisterSerializr(data = data)
+            
+            if  not serializer.is_valid():
+                return Response({
+                    'data':serializer.errors,
+                    'message':'Something went wrong',
+
+                },status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({
+                'data': serializer.data,
+                'message': 'Your account is created'
+            },status=status.HTTP_201_CREATED)
+        except Exception as e:
+
+            return Response({
+                'data':{},
+                'message':'Something went wrong',
+
+            },status=status.HTTP_400_BAD_REQUEST)
+
+
+
