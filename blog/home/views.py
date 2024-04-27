34fd759472280955c5404a2922446dec7384a84b
Kabhilan <kabhilan05@gmail.com>
2024-04-27 18:25:51 +0530

    Get method and  search filter is applied

--- blog/home/views.py
+++ blog/home/views.py
@@ -5,6 +5,8 @@ from .serializers import BlogSerializer
 from rest_framework import status 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Blog
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,6 +15,30 @@ class BlogView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
+
+    def get(self,request):
+        try:
+            blogs = Blog.objects.filter(user= request.user)
+
+            if request.GET.get('search'):
+                search = request.GET.get('search')
+                blogs = blogs.filter(Q(title__icontains=search)|Q(blog_text__icontains=search))
+
+
+            serializer = BlogSerializer(blogs,many = True)
+
+            return Response({
+                'data': serializer.data,
+                'message':'Blogs fetched successfully'
+            },status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            print(e)
+            return Response({
+                'data':{},
+                'message':'Something went Wrong'
+            },status=status.HTTP_400_BAD_REQUEST)
+        
 
     def post(self, request):
         try:
