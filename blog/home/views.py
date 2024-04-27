8cd99687f56d3d0b2095203d1e37b362dfa26066
Kabhilan <kabhilan05@gmail.com>
2024-04-27 23:23:37 +0530

    completed delet method, added pagination,added random post display

--- blog/home/views.py
+++ blog/home/views.py
@@ -7,8 +7,38 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Blog
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+
+class PublicBlog(APIView):
+    def get(self,request):
+        try:
+            blogs = Blog.objects.all().order_by('?')
+
+            if request.GET.get('search'):
+                search = request.GET.get('search')
+                blogs = blogs.filter(Q(title__icontains=search)|Q(blog_text__icontains=search))
+            
+
+            page_number =request.GET.get('page',1)
+            paginator = Paginator(blogs,1)
+            serializer = BlogSerializer(paginator.page(page_number),many = True)
+
+            return Response({
+                'data': serializer.data,
+                'message':'Blogs fetched successfully'
+            },status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            print(e)
+            return Response({
+                'data':{},
+                'message':'Something went Wrong or Invalid page'
+            },status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class BlogView(APIView):
 
@@ -104,6 +134,39 @@ class BlogView(APIView):
 
 
         except Exception as e:
+
+            return Response({
+                'Data':{},
+                'Message':'Something went wrong'
+            })
+        
+
+    def delete(self,request):
+        try:
+            data = request.data
+            blog = Blog.objects.filter(uid=data.get('uid'))
+
+            if not blog.exists():
+                return Response({
+                    'data':{},
+                    'message':'invalid blog uid'
+                },status=status.HTTP_400_BAD_REQUEST)
+            if request.user !=blog[0].user:
+                return Response({
+                    'data':{},
+                    'message':'You are not authorised to edit this'
+                },status=status.HTTP_400_BAD_REQUEST)
+            
+            blog[0].delete()
+
+            return Response({
+                'data':{},
+                'message':'Successfully deleted'
+            },status=status.HTTP_200_OK)
+            
+
+        except Exception as e:
+            print(e)
 
             return Response({
                 'Data':{},
